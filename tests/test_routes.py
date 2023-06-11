5271a0be17a142b2ae789cc05f91df186416e5c3
NinjaNas <ktran.business0@gmail.com>
2023-06-11 18:43:19 -0400

    error

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -182,4 +182,11 @@ class TestAccountService(TestCase):
         # get the data from resp.get_json()
         data = resp.get_json()
         # assert that the len() of the data is 5 (the number of accounts you created)
-        self.assertEqual(len(data), 5)  
+        self.assertEqual(len(data), 5)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        # call self.client.delete() on the BASE_URL
+        resp = self.client.delete(BASE_URL)
+        # assert that the resp.status_code is status.HTTP_405_METHOD_NOT_ALLOWED
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
